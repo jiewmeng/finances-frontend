1f4199b2aad36f086a17c517ee6ca2bddd774c05
Jiew Meng <jiewmeng@gmail.com>
2017-03-19 12:16:16 +0800

    Integrate eslint with react

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import app from '../css/app.css'
+import '../css/app.css'
 
 export default class App extends React.Component {
   render() {
     return (
-      <h1 styleName="app.body">Hello World!</h1>
+      <h1 styleName="body">Hello World!</h1>
     )
   }
 }
