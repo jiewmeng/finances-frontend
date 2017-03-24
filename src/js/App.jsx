7c070356fd826d706d24881f6554905ef2b023d5
Jiew Meng <jiewmeng@gmail.com>
2017-03-24 16:48:54 +0800

    Refactored reducer and fix hot reload error

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -12,15 +12,22 @@ import {
 } from "react-toolbox"
 import Navigation from "react-toolbox/lib/navigation"
 
-import financeApp from "./reducers"
+import rootReducer from "./reducers"
 import SbpInvestmentsIndex from "./sbpInvestments/SbpInvestmentsIndex.jsx"
 import Http404 from "./Http404.jsx"
 import "./app.css"
 
+if (module.hot) {
+  module.hot.accept("./reducers", () => {
+    const nextReducer = require("./reducers")
+    store.replaceReducer(nextReducer)
+  })
+}
+
 const history = createHistory()
 const historyMiddleware = routerMiddleware(history)
 const store = createStore(
-  financeApp,
+  rootReducer,
   applyMiddleware(historyMiddleware)
 )
 
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -2,9 +2,9 @@ import {combineReducers} from "redux"
 import sbpInvestments from "./sbpInvestments"
 import {routerReducer} from "react-router-redux"
 
-const financeApp = combineReducers({
+const rootReducer = combineReducers({
   sbpInvestments,
   router: routerReducer
 })
 
-export default financeApp
+export default rootReducer
